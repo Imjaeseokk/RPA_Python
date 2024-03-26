c9e14d8302fbd98c43bd7466db50ff9af7d953fb
Jaeseokk <kakaokokoa9971@gmail.com>
2024-03-26 23:25:14 +0900

    Update mailing.py

--- Mailing/mailing.py
+++ Mailing/mailing.py
@@ -16,8 +16,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 
